e7b5b7b4357a3273102c915d29995455cba9cfcb
HassanBahati <mukisabahati@gmail.com>
2022-09-21 23:22:07 +0300

    add input and out put

---
+++ BinarySearch.py
@@ -0,0 +1,10 @@
+# STEPS
+# 1 start at the middle of a sorted list
+# 2 compare middle value to target, 
+# 3 if middle value matches target, return middle value and terminate
+# 3
+# 4
+# 5
+
+# Input - sorted list 
+# Output - position of target value, or value to show it doesnt exist
