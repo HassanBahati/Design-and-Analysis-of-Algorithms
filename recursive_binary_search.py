7a8ac76aa72a1d7004830ae7e9b6afe2f36ef6b6
HassanBahati <mukisabahati@gmail.com>
2022-09-22 01:52:13 +0300

    add verify function

--- recursive_binary_search.py
+++ recursive_binary_search.py
@@ -13,3 +13,19 @@ def recursive_binary_search(list,target):
                 return recursive_binary_search(list[: midpoint], target)
 
 
+
+
+def verify(index):
+    if index is not None:
+        print("Target found at index: ", index)
+    else:
+        print("Target not found in list")
+
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+result = recursive_binary_search(numbers, 12)
+verify(result)
+
+result = recursive_binary_search(numbers, 6)
+verify(result)
