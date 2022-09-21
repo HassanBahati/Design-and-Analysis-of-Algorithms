046aba04598a23dd8399540b75d3e6fc30414f75
HassanBahati <mukisabahati@gmail.com>
2022-09-22 01:51:02 +0300

    add recursive binary search

---
+++ recursive_binary_search.py
@@ -0,0 +1,15 @@
+def recursive_binary_search(list,target):
+    if len(list) == 0:
+        return False
+    else:
+        midpoint = (len(list)) // 2
+
+        if list[midpoint] == target:
+            return True
+        else:
+            if list[midpoint] < target:
+                return recursive_binary_search(list[midpoint+1 :], target)
+            else:
+                return recursive_binary_search(list[: midpoint], target)
+
+
