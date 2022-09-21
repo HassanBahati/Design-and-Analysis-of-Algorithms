6348bf961ebe55e0e95a3e8c62d01e2b7a6bf4ce
HassanBahati <mukisabahati@gmail.com>
2022-09-21 23:33:52 +0300

    update type of list

--- LinearSearch.py
+++ LinearSearch.py
@@ -1,11 +1,11 @@
 # linear search - sequential search or simple search
 
 # STEPS
-# 1 start at beginning of sorted list 
+# 1 start at beginning of sorted/unsorted list 
 # 2 compare current value and target value
 # 3 if current value is target, return current value and terminate
 # 4 if current value is not target, move to next value 
 # repeate step 2
 
-# Input - set of sorted values 
+# Input - set of sorted/unsorted values 
 # Output - value that matches one being looked for 
