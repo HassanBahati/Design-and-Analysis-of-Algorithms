1f3caf7a57fd0992915f7c4045722e43f3910c8e
HassanBahati <mukisabahati@gmail.com>
2022-09-21 22:55:21 +0300

    update steps

--- LinearSearch.py
+++ LinearSearch.py
@@ -1,11 +1,11 @@
 # linear search - sequential search or simple search
 
 # STEPS
-# 1 start at beginning of list 
+# 1 start at beginning of sorted list 
 # 2 compare current value and target value
 # 3 if current value is target, terminate
 # 4 if current value is not target, move to next value 
 # repeate step 2
 
-# Input - set of values 
+# Input - set of sorted values 
 # Output - value that matches one being looked for 
