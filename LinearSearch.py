8fb7ccdfc2c61d91466fee28b27a833acb13adaa
HassanBahati <mukisabahati@gmail.com>
2022-09-21 22:54:09 +0300

    update sudo code

--- LinearSearch.py
+++ LinearSearch.py
@@ -1,7 +1,7 @@
 # linear search - sequential search or simple search
 
 # STEPS
-# 1 start at first value in the list
+# 1 start at beginning of list 
 # 2 compare current value and target value
 # 3 if current value is target, terminate
 # 4 if current value is not target, move to next value 
