55942f789a80d347dc6956806b59104331664269
HassanBahati <mukisabahati@gmail.com>
2022-09-21 22:50:36 +0300

    add steps required

--- LinearSearch.py
+++ LinearSearch.py
@@ -1,1 +1,8 @@
 # linear search - sequential search or simple search
+
+# STEPS
+# 1 start at first value in the list
+# 2 compare current value and target value
+# 3 if current value is target, terminate
+# 4 if current value is not target, move to next value 
+# repeate step 2
