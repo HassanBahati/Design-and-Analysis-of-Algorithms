7a6b8f1ebadb8d3a7b07b577d6beb146cc7515b8
HassanBahati <mukisabahati@gmail.com>
2022-09-22 23:23:27 +0300

    add merge sort description

---
+++ merge_sort.py
@@ -0,0 +1,7 @@
+# Merge Sort - works like binary search - split problem into sub problems 
+# 1 start with unsorted list 
+# 2 split the list into 2 sub lists
+# 3 keep spliting evenly until 1 element lists are attained 
+# 4 work backwards 
+# 5 repeatedly merge single element lists and sort them 
+
