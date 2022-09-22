ff8deb0d2a44bd6f9b2d71af4f1e8c856c69215a
HassanBahati <mukisabahati@gmail.com>
2022-09-22 23:37:36 +0300

    add split method

--- merge_sort.py
+++ merge_sort.py
@@ -5,3 +5,39 @@
 # 4 work backwards 
 # 5 repeatedly merge single element lists and sort them 
 
+# naively sorting - passing a 1 element list to a merge sort 
+
+
+def merge_sort(list):
+    """
+    Sorts a list in ascending order
+    Returns a new sorted list
+
+    Divide: find the midpoint of the list and divide into sublists
+    Conquer: Recursively sort the sublist created in the previous step
+    Combine: Merge the sorted sublists created in the previous step
+    """
+
+    if len(list) <= 1:
+        return list
+
+    left_half, right_half = split(list)
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
+
+    return merge(left, right)
+
+
+
+def split(list):
+    """
+    Divides the unsorted list at midpoint into sublists
+    Returns two sublists - left and right
+    """
+
+    mid = len(list) // 2
+
+    left = list[:mid] 
+    right = list[mid:]
+
+    return left, right
