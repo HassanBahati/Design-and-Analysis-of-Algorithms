bd8a6196323e848c9e9ff5844032cf90fcc0e3ff
HassanBahati <mukisabahati@gmail.com>
2022-09-22 22:07:18 +0300

    add size and is empty methods

--- linked_list.py
+++ linked_list.py
@@ -8,3 +8,46 @@
 
 # Singly Linked list - contains reference to next node in list 
 # Doubly Linked List - contains reference to both the node previous and next node in the list 
+
+# List traversal - moving from one node to another 
+
+
+# Singly linked list 
+class Node:
+    """
+    An object for storing a single node of a linked list 
+    Models two attributes - data and the link to the next node in the list 
+    """
+    data = None
+    next_node = None
+
+    def __init__(self,data):
+        self.data = data 
+
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
+
+
+class LinkedList:
+    """
+    Singly linked list 
+    """
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def size(self):
+        """
+        Returns the number of nodes in the list 
+        """
+        current = self.head
+        count = 0
+
+        while current:
+            count+=1
+            current = current.next_node
+
+        return count
