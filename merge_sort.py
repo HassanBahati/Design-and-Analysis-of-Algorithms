34e884c755abf07887067ba7f8ff18cfc52843b8
HassanBahati <mukisabahati@gmail.com>
2022-09-22 23:54:50 +0300

    verify merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -70,4 +70,15 @@ def merge(left, right):
         l.append(right[j])
         j += 1
 
-        
+    return l
+
+
+
+
+def verify_sorted(list):
+    n = len(list)
+
+    if n ==0 or n == 1:
+        return True
+
+    return list[0] < list[1] and verify_sorted(list[1:])
