0bf46c44f548945248b006ad71359615ca558faf
HassanBahati <mukisabahati@gmail.com>
2022-09-22 21:50:14 +0300

    describe linked lists

---
+++ linked_list.py
@@ -0,0 +1,10 @@
+# Linked list - linear data structure 
+# Each element in the list is contained in a separate object called a Node 
+# Node - contains individual item of the data and reference to the next node in the list  
+# Head - first node in list 
+# Tail - Last node in list 
+
+# Nodes - are self referential objects - every node contains link to another node
+
+# Singly Linked list - contains reference to next node in list 
+# Doubly Linked List - contains reference to both the node previous and next node in the list 
