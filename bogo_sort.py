666ed115daa33f9a3f0501d729e64d06e48035eb
HassanBahati <mukisabahati@gmail.com>
2022-09-25 17:32:54 +0300

    implement bogo sort

--- bogo_sort.py
+++ bogo_sort.py
@@ -10,4 +10,17 @@ import sys
 
 numbers = [5,8,1,4,7]
 
-print(numbers)
+# check if the list is sorted
+def is_sorted(values):
+    for index in range(len(values) - 1):
+        if values[index] > values[index +1 ]:
+            return False
+    return True
+
+
+def bogo_sort(values):
+    while not is_sorted(values):
+        random.shuffle(values)
+    return values
+
+print(bogo_sort(numbers))
