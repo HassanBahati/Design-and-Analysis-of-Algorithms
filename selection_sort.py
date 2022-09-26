b67a4f8e6ef4fc260e6888f33dc1df0da9b54363
HassanBahati <mukisabahati@gmail.com>
2022-09-26 08:39:03 +0300

    describe selection sort

--- selection_sort.py
+++ selection_sort.py
@@ -1,6 +1,10 @@
 """
 - randomizes the list and keeps checking how far it is from solution
 - slow but atleast better than bogo sort
+
+STEPS 
+1. starts with 2 lists , unsorted list and an empty sorted list
+2. check through all values in list and keep 
 """
 
 import random 
