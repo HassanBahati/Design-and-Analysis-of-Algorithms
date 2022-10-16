f9c6479557f96302536317de20905133d9a8c3db
HassanBahati <mukisabahati@gmail.com>
2022-10-16 15:35:07 +0300

    add traverse array

---
+++ arrays.py
@@ -0,0 +1,14 @@
+'''
+Arrays-container that hold fixed number of items of same type
+
+structure
+array(typecode, [intializers])
+'''
+
+from array import *
+array1 = array('i', [10,20,30,40,50])
+
+#tranverse array
+for x in array1:
+    print(x)
+
